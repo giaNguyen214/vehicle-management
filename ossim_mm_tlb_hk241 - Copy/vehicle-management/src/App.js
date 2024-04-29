b8afdaae3e5e8e060f1c89ce85a2ececfbdf3f71
giaNguyen214 <156935874+giaNguyen214@users.noreply.github.com>
2024-04-29 11:06:06 +0700

    Add files via upload

---
+++ ossim_mm_tlb_hk241 - Copy/vehicle-management/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <a href='/'>Home page</a>
+          </li>
+
+          <li>
+            <a href='/Vehicle'>vehicle</a>
+          </li>
+
+          <li>
+            <a href='/Login'>Login</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
---
+++ ossim_mm_tlb_hk241 - Copy/vehicle-management/src/pages/Vehicle/index.js
@@ -0,0 +1,7 @@
+function Vehicle() {
+    return (
+        <h1>Vehicle</h1>
+    )
+}
+
+export default Vehicle;
